469f6704de2e7b292d57072f8a1034d8f5f1204c
Andrew <dronxix@gmail.com>
2024-05-12 12:12:40 +0300

    Revert "Add files via upload"
    This reverts commit 3713a924c569f2276b8fb408cee93483fee221a7.

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.getElementById("ask-form").addEventListener("submit", function() {
+    document.getElementById("loading-container").style.display = "flex";
+});
+
+document.getElementById("update-gpt-form").addEventListener("submit", function() {
+    document.getElementById("loading-container").style.display = "flex";
+});
